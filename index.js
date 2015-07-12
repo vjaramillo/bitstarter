c08401ce8709dde2047efdc5b9dc1e3d45e8f855
vjaramillo <jaramillovelasquez@gmail.com>
2015-07-12 03:24:01 +0000

    modificaciones en index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
+var content;
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('carenalga')
+    content = fs.readFileSync('index.html', 'utf8');
+    response.send(content);
 })
 
 app.listen(app.get('port'), function() {
